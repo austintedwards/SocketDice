d456a9dc7caaedad82ccf46e87802a7f9e8f0aa5
Austin Edwards <austintedwards@gmail.com>
2017-04-05 11:40:14 -0600

    Beginning with sockets

--- LiarsDice/src/pages/home/home.ts
+++ LiarsDice/src/pages/home/home.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as io from 'socket.io-client';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  socket:any
+  chat_input:string;
+  chats = [];
 
   constructor(public navCtrl: NavController) {
+    this.socket = io('http://localhost:5000');
 
+    this.socket.on('message', (msg) => {
+      console.log("message", msg);
+      this.chats.push(msg);
+    });
   }
 
+  send(msg) {
+        if(msg != ''){
+            this.socket.emit('message', msg);
+        }
+        this.chat_input = '';
+    }
+    // newGame() {
+    //   let modal = this.modalCtrl.create(NewGamePage);
+    //   modal.onDidDismiss(game => {
+    //     if(game){
+    //       // this.gamedata.createGame(game);
+    //     }
+    //   });
+    //   modal.present()
+    // }
+    //
+    // joinGame() {
+    //   let modal = this.modalCtrl.create(JoinGamePage);
+    //   modal.onDidDismiss(game => {
+    //     if(game){
+    //       // this.gamedata.addPlayer(game);
+    //     }
+    //   });
+    //   modal.present()
+    // }
 }
